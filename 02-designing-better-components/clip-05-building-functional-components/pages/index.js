f1fc4824e0436fd4323910a298ea44ec535aa5cf
Ryan Carroll <r.carroll@nationwide.com>
2022-05-11 14:44:02 -0400

    more componetization

--- 02-designing-better-components/clip-05-building-functional-components/pages/index.js
+++ 02-designing-better-components/clip-05-building-functional-components/pages/index.js
@@ -1,5 +1,4 @@
-import { data } from "../SpeakerData";
-import Speaker from "../src/components/Speaker"
+import Speakers from "../src/components/Speakers"
 
 
 
@@ -7,27 +6,7 @@ import Speaker from "../src/components/Speaker"
 const IndexPage = () => {
 
   return (
-    <div className="container speakers-list">
-      <div className="row">
-        {data.map(function (speaker) {
-          const {
-            id,
-            bio,
-            first,
-            last,
-            favorite,
-            twitterHandle,
-            company,
-            sessions
-          } = speaker;
-
-          return (
-            <Speaker key={speaker.id} speaker={speaker} />
-          );
-        })}
-       
-      </div>
-    </div>
+    <Speakers />
   );
 };
 
---
+++ 02-designing-better-components/clip-05-building-functional-components/src/components/Speakers.js
@@ -0,0 +1,13 @@
+
+import { data } from "../../SpeakerData";
+import SpeakersList from "./SpeakersList";
+
+function Speakers() {
+    return (
+        <div className="container-fluid">
+            <SpeakersList data={data} />
+        </div>
+    );
+}
+
+export default Speakers;
